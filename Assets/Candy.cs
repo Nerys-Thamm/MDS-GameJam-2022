f9f653fb7d6464e1c2413727cf180f4b6921bf67
Ashurri <Ashley.rickit@gmail.com>
2022-01-29 20:58:38 +1300

    Candy Disappears

---
+++ Assets/Candy.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    public float CandyRadius = 5.0f;
+    public LayerMask NPCmask;
+
+    public float CandyLifeTime = 0.0f;
+    public float MaxCandyLifeTime = 40.0f;
+    void CheckAreKidsInRange()
+    {
+        Collider[] hitNPCS = Physics.OverlapSphere(transform.position, CandyRadius, NPCmask);
+
+        float decayMultiplier = 0.0f;
+        if(hitNPCS.Length > 0)
+        {
+            for (int i = 0; i < hitNPCS.Length; i++)
+            {
+                decayMultiplier += 1.0f;
+
+            }
+
+            CandyLifeTime -= decayMultiplier * Time.deltaTime;
+        }
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, CandyRadius);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        CandyLifeTime = MaxCandyLifeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (CandyLifeTime <= 0)
+        {
+            Destroy(gameObject);
+        }
+        CheckAreKidsInRange();
+    }
+}
