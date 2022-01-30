244341992b8076883de665f6b86995814126eca5
Nerys-Thamm <nerysthamm@mgail.com>
2022-01-30 16:29:53 +1300

    added kids

--- Assets/Ashs_Scripts/UIManager.cs
+++ Assets/Ashs_Scripts/UIManager.cs
@@ -2,8 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
-
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
 }
